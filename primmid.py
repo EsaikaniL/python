e3d8ec1983a257aec26b79a669da7abedcd9985b
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-20 10:10:19 +0530

    Update and rename primmid.py to armmid.py

--- primmid.py
+++
@@ -1,9 +0,0 @@
-a=int(input())
-b=int(input())
-for i in range(a,b + 1):
-   if i > 1:
-       for j in range(2,i):
-           if (i % j) == 0:
-               break
-       else:
-           print(i)
